9ef77603ad63c186d9e7ed7d3e0f4fa2dd7a3381
Sudharsan Sukumar <sudharsan.sukumar5@gmail.com>
2017-11-07 04:59:08 +0000

    Fix .gitignore not working for committed docs and dist

--- dist/transports/http-transport.d.ts
+++
@@ -1,9 +0,0 @@
-import { GenericTransport } from '@digilent/linx-device-js';
-export declare class HttpTransportService extends GenericTransport {
-    address: string;
-    endpoint: string;
-    start: number;
-    finish: number;
-    constructor(address: string, endpoint: string);
-    writeRead(data: any): Promise<any>;
-}
